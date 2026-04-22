10d63093457093764bf9684b7ae266e3e7199dfe
minhmannh2001 <81837992+minhmannh2001@users.noreply.github.com>
2026-04-22 07:28:23 +0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        
+        @lru_cache(None)
+        def dfs(start):
+            if start == len(s):
+                return True
+            
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in wordSet and dfs(end):
+                    return True
+            
+            return False
+        
+        return dfs(0)
