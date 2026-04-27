46d32dd91f3d6219e0a0ba1c5ae252f0cda3dcfc
minhmannh2001 <81837992+minhmannh2001@users.noreply.github.com>
2026-04-27 13:32:36 +0700

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        @lru_cache(None)
+        def dfs(i, current):
+            # đã dùng hết nums
+            if i == len(nums):
+                return 1 if current == target else 0
+            
+            # chọn + hoặc -
+            return dfs(i + 1, current + nums[i]) + \
+                   dfs(i + 1, current - nums[i])
+        
+        return dfs(0, 0)
