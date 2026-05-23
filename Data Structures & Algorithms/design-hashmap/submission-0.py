423eb52930c128fa47958dcdc884ab9ed48bc62e
minhmannh2001 <81837992+minhmannh2001@users.noreply.github.com>
2026-05-23 13:41:16 +0700

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,49 @@
+class MyHashMap:
+
+    def __init__(self):
+
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+
+    def hash(self, key: int) -> int:
+        return key % self.size
+
+
+    def put(self, key: int, value: int) -> None:
+
+        index = self.hash(key)
+        bucket = self.buckets[index]
+
+        for i, (k, v) in enumerate(bucket):
+
+            if k == key:
+                bucket[i] = (key, value)
+                return
+
+        bucket.append((key, value))
+
+
+    def get(self, key: int) -> int:
+
+        index = self.hash(key)
+        bucket = self.buckets[index]
+
+        for k, v in bucket:
+
+            if k == key:
+                return v
+
+        return -1
+
+
+    def remove(self, key: int) -> None:
+
+        index = self.hash(key)
+        bucket = self.buckets[index]
+
+        for i, (k, v) in enumerate(bucket):
+
+            if k == key:
+                bucket.pop(i)
+                return
